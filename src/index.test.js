96b6c4be797c1c38f966f1a545e6c00309f3b738
Joseph Reed <jereed559@gmail.com>
2021-05-30 16:52:44 -0700

    remove snapshot test

--- src/index.test.js
+++ src/index.test.js
@@ -2,11 +2,11 @@ import {render} from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import { CodeEditor } from './components/code-editor';
  
-it('should take a snapshot', () => {
-   const { asFragment } = render(<CodeEditor />)
+// it('should take a snapshot', () => {
+//    const { asFragment } = render(<CodeEditor />)
     
-   expect(asFragment(<CodeEditor />)).toMatchSnapshot()
-   });
+//    expect(asFragment(<CodeEditor />)).toMatchSnapshot()
+//    });
 
 it('renders without crashing', () => {
    const div = document.createElement('div');
